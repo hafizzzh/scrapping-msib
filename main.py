18a9f3ba0f42f21cb612cd7b6b3e3ada41e88ccc
hafizzzh <ibrahimbaim13@gmail.com>
2023-02-10 08:02:18 +0700

    success version 2: method, new column kriteria

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import csv
 
 write = csv.writer(open('result/{}.csv'.format('hasil'), 'w', newline=''))
-header = ['Role', 'Divisi', 'Perusahaan', 'Lokasi', 'Slot']
+header = ['Role', 'Divisi', 'Perusahaan', 'Lokasi', 'Kriteria', 'Slot']
 write.writerow(header)
 
 url = 'https://api.kampusmerdeka.kemdikbud.go.id/magang/browse/position'
@@ -13,17 +13,30 @@ parameter = {
     'order': 'desc'
 }
 
-r = requests.get(url, params=parameter).json()
-count = 0
-loker = r['data']
-for l in loker:
-    role = l['name']
-    divisi = l['activity_name']
-    perusahaan = l['mitra_name']
-    lokasi = l['location']
-    slot = l['total']
-    count += 1
-    print('Nomor: ', count, 'Role: ', role, 'Perusahaan: ', perusahaan)
-    write = csv.writer(open('result/{}.csv'.format('hasil'), 'a', newline=''))
-    data = [role, divisi, perusahaan, lokasi, slot]
-    write.writerow(data)
+def get_data(a):
+    b = requests.get(a).json()
+    b = b['data']
+    return b
+
+def get_data_param(a, p):
+    b = requests.get(a, params=p).json()
+    b = b['data']
+    return b
+
+def main():
+    loker = get_data_param(url, parameter)
+    for l in loker:
+        link = url+'/'+l['id']
+        k = get_data(link)
+        role = l['name']
+        divisi = l['activity_name']
+        perusahaan = l['mitra_name']
+        lokasi = l['location']
+        req = k['requirement']
+        slot = l['total']
+
+        write = csv.writer(open('result/{}.csv'.format('hasil'), 'a', newline=''))
+        data = [role, divisi, perusahaan, lokasi, req, slot]
+        write.writerow(data)
+
+main()
